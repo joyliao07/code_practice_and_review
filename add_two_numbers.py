e7fd04d5d2769623cd51b5974092a6b851945655
Joyce Liao <joyliao07@gmail.com>
2019-03-09 22:21:36 -0800

    add_two_numbers challenge

---
+++ add_two_numbers.py
@@ -0,0 +1,31 @@
+# Exercise: Add Two Numbers
+
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+
+
+############################################
+####          TO PRINT SOLUTIONS:       ####
+####                                    ####
+############################################
+
+# nums = [2, 7, 11, 15]
+# target = 9
+# solution = Solution()
+# print(solution.twoSum(nums, target))
