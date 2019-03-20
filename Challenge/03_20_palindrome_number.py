b9cb1d53d479c8720b557aa0311d66790ee9ea36
Joyce Liao <joyliao07@gmail.com>
2019-03-20 13:58:50 -0700

    solution

--- Challenge/03_20_palindrome_number.py
+++ Challenge/03_20_palindrome_number.py
@@ -1,7 +1,10 @@
 # Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
 
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-
-
-
+        y = str(x)
+        if y == y[::-1]:
+            return True
+        else:
+            return False
