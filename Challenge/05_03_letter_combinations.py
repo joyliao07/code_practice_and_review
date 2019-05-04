fc4f502f805920c80a38f4ca2d6ee022bc2dcb3a
Joyce Liao <joyliao07@gmail.com>
2019-05-03 20:35:08 -0700

    test solution

--- Challenge/05_03_letter_combinations.py
+++ Challenge/05_03_letter_combinations.py
@@ -6,6 +6,36 @@
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        lst = []
+        res = []
+        for num in digits:
+            if num == "2":
+                lst.append(['a', 'b', 'c'])
+            elif num == "3":
+                lst.append(['d', 'e', 'f'])
+            elif num == "4":
+                lst.append(['g', 'h', 'i'])
+            elif num == "5":
+                lst.append(['j', 'k', 'l'])
+            elif num == "6":
+                lst.append(['m', 'n', 'o'])
+            elif num == "7":
+                lst.append(['p', 'q', 'r', 's'])
+            elif num == "8":
+                lst.append(['t', 'u', 'v'])
+            elif num == "9":
+                lst.append(['w', 'x', 'y', 'z'])
+
+        # lst = [['a', 'b', 'c'], ['d', 'e'], ['f', 'g', 'h']]
+        for i in range(len(lst[0])):
+          for j in range(len(lst[1])):
+            for k in range(len(lst[2])):
+              combo = lst[0][i]
+              combo += lst[1][j]
+              combo += lst[2][k]
+              res.append(combo)
+            
+        return res
 
 
 
