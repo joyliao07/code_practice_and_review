e1796134e584ee7152fc0044456871662afda686
Joyce Liao <joyliao07@gmail.com>
2019-05-03 19:22:21 -0700

    setup

---
+++ Challenge/05_03_letter_combinations.py
@@ -0,0 +1,27 @@
+# Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+# A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+# Input: "23"
+# Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
