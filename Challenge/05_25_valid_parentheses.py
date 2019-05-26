5365c9886f5f75947351857057aebd527565c340
Joyce Liao <joyliao07@gmail.com>
2019-05-25 22:32:01 -0700

    solution

--- Challenge/05_25_valid_parentheses.py
+++ Challenge/05_25_valid_parentheses.py
@@ -6,8 +6,41 @@
 # Open brackets must be closed in the correct order.
 # Note that an empty string is also considered valid.
 
+
 class Solution:
     def isValid(self, s: str) -> bool:
+        temp = ''
 
+        for what in s:
+            if what == '{' or what =='[' or what =='(':
+                temp += what
+            else:
+                if what == '}':
+                    what = '{'
+                    if len(temp) == 0:
+                        return False
+                    elif temp[-1] == what:
+                        temp = temp[:-1]
+                    else:
+                        return False
+                elif what == ']':
+                    what = '['
+                    if len(temp) == 0:
+                        return False
+                    elif temp[-1] == what:
+                        temp = temp[:-1]
+                    else:
+                        return False
+                elif what == ')':
+                    what = '('
+                    if len(temp) == 0:
+                        return False
+                    elif temp[-1] == what:
+                        temp = temp[:-1]
+                    else:
+                        return False
 
+        if len(temp) != 0:
+            return False
 
+        return True
