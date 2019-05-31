59d6ae0b91fef401f35ee9fe7d040e9e4a8edd94
Joyce Liao <joyliao07@gmail.com>
2019-05-30 23:39:22 -0700

    while loop

--- Challenge/05_30_fibonacci.py
+++ Challenge/05_30_fibonacci.py
@@ -2,6 +2,8 @@
 # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
 
 
+# Recurssion:
+
 def fibonacci(length):
     if(length <= 1):
         return length
@@ -12,9 +14,16 @@ for i in range(length):
     print(fibonacci(i))
 
 
+# While_loop:
 
+x = 0
+y = 1
+iteration = 0
 
-
-
-
-
+if length == 1:
+    print(x)
+else:
+    while iteration < length:
+        print(x, end=', ')
+        x, y = y, (x+y)
+        iteration += 1
