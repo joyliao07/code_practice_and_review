75ca99512330b7e226ac1c8f0422e5cbdccd933f
Joyce Liao <joyliao07@gmail.com>
2019-06-03 23:41:50 -0700

    recursion

--- Challenge/4_sum.py
+++ Challenge/4_sum.py
@@ -2,26 +2,22 @@
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        results = []
+        nums.sort()
+        self.find_sum(nums, target, N, [], results)
+        return results
+
+
+    def find_sum(self, nums, target, N, result, results):
+        if N < len(nums) or N < 2:
+            return
+        if N == 2:
+            # find the pair
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        else:
+            for i in range(len(nums) - N + 1):
+                if num[i] * N > target or num[-1] * N < target:
+                    break
+                self.find_sum(nums[i+1:], target - num[i], N-1, result+[num[i]], results)
