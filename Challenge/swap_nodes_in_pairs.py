f1141d6f69f36a7e0527fe4a66c1131999cf7939
Joyce Liao <joyliao07@gmail.com>
2019-06-15 17:05:10 -0700

    setup

---
+++ Challenge/swap_nodes_in_pairs.py
@@ -0,0 +1,36 @@
+# Given a linked list, swap every two adjacent nodes and return its head.
+
+# You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
