2d5bb78354f71e61ef6f81f3cd218c0807d2bb52
Joyce Liao <joyliao07@gmail.com>
2019-06-17 18:34:21 -0700

    setup

---
+++ Challenge/remove_element.py
@@ -0,0 +1,28 @@
+# Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+# Given nums = [3,2,2,3], val = 3,
+
+# Your function should return length = 2, with the first two elements of nums being 2.
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
