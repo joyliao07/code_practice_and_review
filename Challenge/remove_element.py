e9b59835c03a0834cbc472d5b181b5f7d145d9a4
Joyce Liao <joyliao07@gmail.com>
2019-06-20 21:19:31 -0700

    solution #4

--- Challenge/remove_element.py
+++ Challenge/remove_element.py
@@ -33,3 +33,21 @@ class Solution:
             i += 1
         return nums
 
+    def removeElement_4(self, nums: List[int], val: int) -> int:
+        """Return only the length of nums. Runtime 84%; Memory 17%"""
+        if len(nums) == 0:
+            return 0
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+        return j
+
+
+
+
+
+
+
+
